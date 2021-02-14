1ab59b1fbd948c370170eb347997041a50717096
Nermin Kaya <nermin.kaya.sau@gmail.com>
2021-02-14 12:21:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,33 @@
 from flask import Flask, request
-import flask
 import redis
-import requests
-import json
+
 
 app = Flask(__name__)
 
 app.debug = True
-db = redis.Redis("localhost",decode_responses=True)  # connect to server
+rds = redis.Redis("localhost", decode_responses=True)  # connect to server
 
 
 @app.route('/')
 def hello():
     return 'Welcome to my application :) '
 
-@app.route("/keys/<value>/", methods=["HEAD"])
-def head_request(value=None):
+
+@app.route("/HEAD/keys/<id>/", methods=["HEAD"])
+def head(id=None):
     """
     check value is exist in database or not
 
-    :param value: [check if exist or not], defaults to None
-    :type value: [type], optional
+    :param id: [check if exist or not], defaults to None
+
     """
-    if db.exists(value):
-        return flask.jsonify("Exists"), requests.status_codes.codes.found
-    return flask.jsonify("Not Found"), requests.status_codes.codes.not_found
+    if rds.exists(id):
+        return "Exists", 302
+    return "Not Found", 404
 
 
-@app.route("/keys", methods=["GET"])
-@app.route("/keys/<value>/", methods=["GET"])
-def get_request(value=None):
-    """
-    Get sepcific key or all keys in redis database
-    """
-    data = dict()
-    filter_keys = request.args.get("filter", default="*", type=str)
-    if value is None:
-        for i in db.scan_iter(match=filter_keys.replace("$", "*")):
-            data[i] = db.get(i)
-
-    else:
-        data[value] = db.get(value)
-        if data is None:
-            return flask.jsonify(data), requests.status_codes.codes.not_found
-
-    return flask.jsonify(data), requests.status_codes.codes.ALL_OKAY
-
-
-
-@app.route("/keys", methods=["PUT"])
-def put_request():
+@app.route("/PUT/keys", methods=["PUT"])
+def put():
     """
     put key and value to redis database and with expire time
     """
@@ -57,23 +35,42 @@ def put_request():
     expire = request.args.get("expire_in", default=31104000, type=int)  # one year
     for key, value in data.items():
         print(f"{key}:{value}")
-        db.set(key, value, ex=expire)
-    return flask.jsonify(data), requests.status_codes.codes.CREATED
+        rds.set(key, value, ex=expire)
+    return data, 201
 
 
-@app.route("/keys/<value>/", methods=["DELETE"])
-@app.route("/keys", methods=["DELETE"])
-def delete_request(value=None):
+@app.route("/GET/keys", methods=["GET"])
+@app.route("/GET/keys/<id>/", methods=["GET"])
+def get(id=None):
     """
-    Delete all keys from redis database or specific keys depand on value
+    Get sepcific key or all keys in redis database
     """
-    if value is None:
-        for val in db.scan_iter(match="*"):
-            db.delete(val)
+    data = dict()
+    filter_keys = request.args.get("filter", default="*", type=str)
+    if id is None:
+        for i in rds.scan_iter(match=filter_keys.replace("$", "*")):
+            data[i] = rds.get(i)
+
     else:
-        db.delete(value)
-    return flask.jsonify("Deleted"), requests.status_codes.codes.ALL_OKAY
+        data[id] = rds.get(id)
+        if data is None:
+            return data, 404
 
+    return data, 200
+
+
+@app.route("/DELETE/keys/<id>/", methods=["DELETE"])
+@app.route("/DELETE/keys", methods=["DELETE"])
+def delete(id=None):
+    """
+    Delete all keys from redis database or specific keys dependence on value
+    """
+    if id is None:
+        for val in rds.scan_iter(match="*"):
+            rds.delete(val)
+    else:
+        rds.delete(id)
+    return "Deleted", 200
 
 
 if __name__ == "__main__":
